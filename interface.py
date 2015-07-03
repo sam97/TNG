df874b847bcf1659608dc5dc0c28e5dcbc39cadc
Sam Glider <smahaseeb@hotmail.com>
2015-07-03 15:16:32 +0530

    Added some documentation/comments.
    Cleared the useless, commented code snippets. Added some comments on the
    workings.

--- interface.py
+++ interface.py
@@ -7,160 +7,122 @@ import praw
 # PRAW Initialisation
 r = praw.Reddit(user_agent='the-number-graph by /u/glider97')
 
-f1 = open("latest.txt", 'r')
-latest = f1.read().split('\n')
-f1.close()
-f2 = open("details.txt", "a+")
+f1 = open("latest.txt", 'r')    # latest.txt stores the ids of the last comment and thread used.
+latest = f1.read().split('\n')  # They are stored in a list called 'latest'
+f1.close()                      # 'details.txt' stores a log of all the comments "approved".
+f2 = open("details.txt", "a+")  #
 
-# File Legend:
+# latest.txt Legend:
 #	0 - curThreadID
 #	1 - curComID
-#	2 - Array of comments (feature abolished)
 
 curThreadID=latest[0] # ID of the Number Thread
 curComID=latest[1] # First comment's ID in the Number Thread.
 comms=[] # List of comments saved.
 
 f2.write("========================================================\n")
-f2.write(str(curThreadID)+'\n')
-# for i in range(2,len(details)-1,4):
-# 	comms.append([])
-# 	for j in range(0,4):
-# 		comms[i-2].append(details[i+j]) ###
-# comms = details[2]
+f2.write(str(curThreadID)+'\n') # Log ID of thread about to be used. Buggy.
 
 def main():
-	# print """
-	# Welcome. Let's begin.
-	# The options available are:
-	# 1. Start/Next
-	# 2. Stop
-	# 3. Migrate
-	# Enter your choice:
-	# """
-	# ch=raw_input()
-	# while (ch!='n'):
-	# 	if ch==1:
-	# 		start()
-	# 	elif ch==2:
-	# 		stop()
-	# 	elif ch==3:
-	# 		migrate()
-	# 	ch=raw_input()
+	# Begin.
 	start()
 
 def migrate():
+	# The function of migrate() is to change the thread ID
+	# (and consequently the comment ID) on which the work is to be done.
+	# See: last comment of The Mona Lisa Thread.
+
 	global curThreadID
 	global curComID
 	global comms
-	curThreadID=raw_input("Enter the Thread ID:")
-	# thread = r.get_submission(submission_id=curThreadID)
-	# curThreadID = thread
-	# thread = praw.helpers.flatten_tree(thread.comments)
-	curComID=raw_input("Enter the Comment ID:")
-	# for i in range(0,len(thread)):
-	# 	if thread[i].id == curComID:
-	# 		curComID = thread[i]
-	# 		postTime = datetime.fromtimestamp(int(curComID.created_utc)).strftime('%Y-%m-%d %H:%M:%S')
-	# 		# print "Comment found at", curComID
-	# 		comms.append([curComID.id, curComID.author, postTime, curComID.body])
-	# 		break
+	curThreadID=raw_input("Enter the Thread ID (ex: 2dwxho):")
+	curComID=raw_input("Enter the Comment ID (ex: cju11n6):")
+
 	try:
 		thread = r.get_submission('http://www.reddit.com/r/explainlikeimfive/comments/' + curThreadID + "/_/" + curComID)
 	except Exception,e:
-		print "Comment not found. Please try again."
+		print "Error! Please try again."
 		print e
 	else:
-		curComID = thread.comments[0]
+		curComID = thread.comments[0] # Make comment ID variable as comment object.
 		postTime = datetime.fromtimestamp(int(curComID.created_utc)).strftime('%Y-%m-%d %H:%M:%S')
-		comms.append([curComID.id, curComID.author, postTime, curComID.body])
-	if type(curComID)==str:
+		comms.append([curComID.id, curComID.author, postTime, curComID.body]) # Append a list of data of the comment.
+	if type(curComID)==str: # If thread or comment not found.... Maybe useless?
 		curComID, curThreadID=''
 		comms=[]
 		print "Comment not found. Please try again."
-		main()
+		main() # Reset and try again.
 
 def start():
+	# The core of the script. Gives an interface to 'store', 'discard', or
+	# 'save' a comment, and to 'migrate' to another thread.
+
 	global curThreadID
 	global curComID
 	global comms
 	char = ''
-	temp = []
-	if curComID=='' or curThreadID=='':
-		migrate()
+	temp = [] # Store the comment data. Later add to comms list.
+	if curComID=='' or curThreadID=='': # If thread or comment ID not found in latest.txt....
+		migrate()                       # go to migrate() and get it.
 	elif type(curComID)==str:
 		thread = r.get_submission('http://www.reddit.com/r/explainlikeimfive/comments/' + curThreadID + "/_/" + curComID)
 		curComID = thread.comments[0]
 		postTime = datetime.fromtimestamp(int(curComID.created_utc)).strftime('%Y-%m-%d %H:%M:%S')
-		comms.append([curComID.id, curComID.author, postTime, curComID.body])
-	# while char!=4:
+		comms.append([curComID.id, curComID.author, postTime, curComID.body]) # Append a list of data of the comment.
 	if len(curComID.replies)==0:
 		print "This is the latest comment yet."
 	else:
+		# Interface begins.
+
 		char = 0
 		x = 0
-		# for x in curComID.replies:
-		# def enlist(id):
-		# 	commsList = []
-		# 	commsList.append(id.replies[0])
-		# 	if len(id.replies) > 1:
-		# 		for i in id.replies[1].comments():
-		# 			commsList.append(i)
-		# 	return commsList
-		# commsList = enlist(curComID)
 		while x < len(curComID.replies):
-			y = curComID.replies[x] # extra added
-			# print "DEBUG FLAG"
-			if type(y)==praw.objects.MoreComments:
+			y = curComID.replies[x]
+			if type(y)==praw.objects.MoreComments: # "load more comments"
 				y = y.comments()[0]
-				# curComID = y.comments()[0]
-				# x=0
-				# continue
-			# print "DEBUG FLAG: type of curComID.replies(x) is", type(y), type(curComID.replies), type(curComID)
 			postTime = datetime.fromtimestamp(int(y.created_utc)).strftime('%Y-%m-%d %H:%M:%S')
 			print "Comment made by", y.author, 'at', postTime
 			print '"' + y.body + '"'
 			char = raw_input("1)Store     2)Discard     3)Store, Save and Migrate     4)Save     5)Exit\n")
 			if char=='1':
-				temp.append([y.id, y.author, postTime, y.body])
+				temp.append([y.id, y.author, postTime, y.body]) # "Approve" the comment. 'Save' later.
 				curComID = y
-				# commsList = enlist(curComID)
 				x=0
 			elif char=='2':
-				x+=1
+				x+=1 # Disapprove/Discard the current comment.
 			elif char=='3':
 				for i in temp:
 					comms.append(i)
 				temp=[]
 				x=0
-				migrate()
-				# commsList = enlist(curComID)
+				migrate() # "Approve", 'save' and move on to another thread.
 			elif char=='4':
-				for i in temp:
-					comms.append(i)
+				for i in temp:      # A code snippet....
+					comms.append(i) # to 'save' the comments "approved".
 				temp=[]
-				# curComID = y
 			elif char=='5':
 				break
-			# x+=1
-		if char != '3' and char!='5':
+		if char != '3' and char!='5': # Bugs???
 			print "The thread has come to an end. This was the latest comment."
 			char = raw_input("Progress Saved!\nDo you want to (m)igrate to a new thread, or (s)top?")
 			for i in temp:
-				comms.append(i)
+				comms.append(i) # 'Save'
 			if char=='m':
 				migrate()
 				main()
+
+		# Interface ends.
 	for i in comms:
-		if i == ['','','','']:
+		if i == ['','','','']: # Remove empty comment data, if any. Maybe useless?
 			comms.remove(i)
-	# f.seek(0,0)
-	f1 = open("latest.txt", 'w')
-	f1.write(str(curThreadID)+'\n'+str(curComID.id)+'\n')
+	f1 = open("latest.txt", 'w')                          # Write the last thread and comment
+	f1.write(str(curThreadID)+'\n'+str(curComID.id)+'\n') # IDs operated upon.
 	f1.close()
 	for i in comms:
-		f2.write(str(i[0])+'\n')
+		f2.write(str(i[0])+'\n') # Log the comment IDs "approved".
 	f2.close()
+	# Placeholder to either return list to 'plot' script....
+	# or call the script itself by parameters.
 
 
 if __name__=='__main__':
